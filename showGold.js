51b0fb38441c537ad04afa467ef4c4e8d33dd1eb
kornieP <kornp.contact@gmail.com>
2024-02-06 12:39:49 +0700

    change graph

--- showGold.js
+++ showGold.js
@@ -1,20 +1,22 @@
-
+const margin1 = {top: 10, right: 30, bottom: 90, left: 40},
+width1 = 1980 - margin1.left - margin1.right,
+height1 = 1080 - margin1.top - margin1.bottom;
 // append the svg object to the body of the page
 const svg1 = d3.select("#graphGoldAll")
 .append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
+.attr("width", width1 + margin1.left + margin1.right)
+.attr("height", height1 + margin1.top + margin1.bottom)
 .append("g")
-.attr("transform", `translate(${margin.left},${margin.top})`);
+.attr("transform", `translate(${margin1.left},${margin1.top})`);
 d3.csv("https://raw.githubusercontent.com/kornieP/goldMedal/main/goldMedal.csv").then( function(data) {
     
 // X axis
 const x = d3.scaleBand()
-  .range([ 0, width ])
+  .range([ 0, width1 ])
   .domain(data.map(function(d) { return d.Year; }))
   .padding(1);
 svg1.append("g")
-  .attr("transform", `translate(0, ${height})`)
+  .attr("transform", `translate(0, ${height1})`)
   .call(d3.axisBottom(x))
   .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-45)")
@@ -23,7 +25,7 @@ svg1.append("g")
 // Add Y axis
 const y = d3.scaleLinear()
   .domain([0, 200])
-  .range([ height, 0]);
+  .range([ height1, 0]);
 svg1.append("g")
   .call(d3.axisLeft(y));
 
