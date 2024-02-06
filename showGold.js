a66dc27ffb3e35dc1cfa84eecdabaff80e8bf3f5
kornieP <kornp.contact@gmail.com>
2024-02-06 12:05:56 +0700

    two graph

--- showGold.js
+++ showGold.js
@@ -1,23 +1,19 @@
 
-const margin = {top: 10, right: 30, bottom: 90, left: 40},
-width = 1000 - margin.left - margin.right,
-height = 1080 - margin.top - margin.bottom;
-
 // append the svg object to the body of the page
-const svg = d3.select("#graphGoldMax")
+const svg1 = d3.select("#graphGoldAll")
 .append("svg")
 .attr("width", width + margin.left + margin.right)
 .attr("height", height + margin.top + margin.bottom)
 .append("g")
 .attr("transform", `translate(${margin.left},${margin.top})`);
-d3.csv("https://raw.githubusercontent.com/kornieP/goldMedal/main/goldMedalMax.csv").then( function(data) {
+d3.csv("https://raw.githubusercontent.com/kornieP/goldMedal/main/goldMedal.csv").then( function(data) {
     
 // X axis
 const x = d3.scaleBand()
   .range([ 0, width ])
   .domain(data.map(function(d) { return d.Year; }))
   .padding(1);
-svg.append("g")
+svg1.append("g")
   .attr("transform", `translate(0, ${height})`)
   .call(d3.axisBottom(x))
   .selectAll("text")
@@ -28,11 +24,11 @@ svg.append("g")
 const y = d3.scaleLinear()
   .domain([0, 200])
   .range([ height, 0]);
-svg.append("g")
+svg1.append("g")
   .call(d3.axisLeft(y));
 
 // Lines
-svg.selectAll("myline")
+svg1.selectAll("myline")
   .data(data)
   .enter()
   .append("line")
@@ -43,7 +39,7 @@ svg.selectAll("myline")
     .attr("stroke", "grey")
 
 // Circles
-svg.selectAll("mycircle")
+svg1.selectAll("mycircle")
   .data(data)
   .join("circle")
     .attr("cx", function(d) { return x(d.Year); })
